2e567262bee454d057a366e0aeda88d5419a8ee6
Dan Katzuv <dan@katzuv.feigin>
2022-05-18 02:14:58 +0300

    Add TBA auth key as option

--- scripts/frc/teams_counter.py
+++ scripts/frc/teams_counter.py
@@ -4,6 +4,17 @@ CONTEXT = {"help_option_names": ["-h", "--help"]}
 
 
 @click.command(context_settings=CONTEXT)
+@click.option(
+    "-k",
+    "--key",
+    "api_key",
+    required=True,
+    prompt=True,
+    hide_input=True,
+    envvar="TBA_AUTH_KEY",
+    show_envvar=True,
+    help="TBA API authorization key",
+)
 def cli():
     pass
 
