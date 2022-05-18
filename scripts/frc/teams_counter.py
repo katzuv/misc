0815d9e96967f27d51d8c930fd90b5c8fa983b47
Dan Katzuv <dan@katzuv.feigin>
2022-05-18 09:51:52 +0300

    Add function that returns all teams

--- scripts/frc/teams_counter.py
+++ scripts/frc/teams_counter.py
@@ -1,4 +1,8 @@
+import itertools
 import string
+import urllib.parse
+
+import requests
 
 
 AUTH_KEY_HEADER = "X-TBA-Auth-Key"
@@ -8,3 +12,22 @@ TEAMS_SIMPLE_ENDPOINT = string.Template("teams/$page_number/simple")
 COUNTRY = "country"
 ISRAEL = "Israel"
 TEAM_NUMBER = "team_number"
+
+
+def get_all_teams(api_key: str) -> list[dict]:
+    """
+    :param api_key: TBA API auth key
+    :return: list of all teams
+    """
+    teams = []
+    for page_number in itertools.count():
+        url = urllib.parse.urljoin(
+            API_BASE_URL, TEAMS_SIMPLE_ENDPOINT.substitute(page_number=page_number)
+        )
+        response = requests.get(url, headers={AUTH_KEY_HEADER: api_key})
+        paged_teams = response.json()
+        if not paged_teams:
+            break
+        teams.extend(paged_teams)
+
+    return teams
