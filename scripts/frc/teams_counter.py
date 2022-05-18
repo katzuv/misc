9d5e90da1c9e0ce5e43430634ed0205625162723
Dan Katzuv <dan@katzuv.feigin>
2022-05-18 03:19:07 +0300

    Add TBA API auth key header constant

--- scripts/frc/teams_counter.py
+++ scripts/frc/teams_counter.py
@@ -1,6 +1,7 @@
 import string
 
 
+AUTH_KEY_HEADER = "X-TBA-Auth-Key"
 API_BASE_URL = "https://www.thebluealliance.com/api/v3/"
 TEAMS_SIMPLE_ENDPOINT = string.Template("teams/$page_number/simple")
 
