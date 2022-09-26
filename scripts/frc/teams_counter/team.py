4fff1c519f1194791f4bce81698459f62eb6c870
Dan Katzuv <dan.dan.kafe@gmail.com>
2022-09-26 15:58:44 +0300

    Add method that returns a pretty version of team year spans

--- scripts/frc/teams_counter/team.py
+++ scripts/frc/teams_counter/team.py
@@ -7,3 +7,18 @@ class Team:
     name: str
     location: str
     year_spans: list[tuple[int, int]]
+
+    def _prettify_year_spans(self) -> str:
+        """
+        :return: prettified year spans the team is active
+        """
+        output_parts = []
+        for span in self.year_spans:
+            if len(span) == 1:
+                year = span[0]
+                output_parts.append(str(year))
+            else:
+                first_year, last_year = span
+                output_parts.append(f"{first_year}-{last_year}")
+
+        return ", ".join(output_parts)
